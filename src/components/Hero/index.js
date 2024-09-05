f89c2c7f1f80bd6da6140a7d72c53ada89fac9b9
rfdevuser <rakhisfashions@rakhisfashions.com>
2024-09-05 14:59:13 +0530

    Add files via upload

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -20,10 +20,10 @@ const Hero = () => {
     <>
    <div>
         <div className="bg-hero  w-full h-full text-black max-h-550px bg-white transform  origin-top overflow-hidden z-0">
-        <div className='flex justify-center bg-[#831843] text-white text-center'> 𝑾𝒆𝒍𝒄𝒐𝒎𝒆 𝒕𝒐 𝑹𝑨𝑲𝑯𝑰𝑺 𝑭𝑨𝑺𝑯𝑰𝑶𝑵𝑺, 𝑾𝒆𝒂𝒓 𝑫𝒊𝒇𝒇𝒆𝒓𝒆𝒏𝒕, 𝑭𝒆𝒆𝒍 𝑺𝒑𝒆𝒄𝒊𝒂𝒍
+        <div className='flex justify-center bg-[#4a044e] text-white text-center'> 𝑾𝒆𝒍𝒄𝒐𝒎𝒆 𝒕𝒐 𝑹𝑨𝑲𝑯𝑰𝑺 𝑭𝑨𝑺𝑯𝑰𝑶𝑵𝑺, 𝑾𝒆𝒂𝒓 𝑫𝒊𝒇𝒇𝒆𝒓𝒆𝒏𝒕, 𝑭𝒆𝒆𝒍 𝑺𝒑𝒆𝒄𝒊𝒂𝒍
         </div> 
       
-      <div className=' bg-[#831843] z-0   '>
+      <div className=' bg-[#4a044e] z-0   '>
       <div className='relative z-10 flex flex-rows  justify-center items-center sm:flex-row sm:justify-center gap-5 '>
         <Link href="https://www.onati-global.com/"><Image
           alt='Onati Global'
@@ -75,18 +75,24 @@ const Hero = () => {
       <div className='absolute text-center w-30'> 
       </div></div>
       
-      <div className='relative z-10  mt-90 flex items-center justify-center' style={{ 
-        backgroundImage: `url('/background/hero_2.png')`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        height:700,
- 
-        zIndex: 0,
-      }}>
+      <div
+  className='relative z-10 flex items-center justify-center'
+  style={{
+    backgroundImage: `url('/background/rakhisfashions11.jpg')`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    height: '100vh', // Adjust the height to a percentage of the viewport height
+    width: '100vw', // Ensure the width takes the full viewport width
+    maxHeight: '700px', // Optional: set a max height to prevent it from growing too large
+    zIndex: 0,
+  }}
+>
+
+
         <div className='flex flex-col '>
-  <div className='text-4xl sm:text-5xl md:text-6xl lg:text-8xl xl:text-9xl bg-clip-text text-transparent bg-gradient-to-t from-[#fcd34d] to-[#ffffff] -mt-20 text-center '> 𝑫𝒆𝒇𝒊𝒏𝒆 𝒀𝒐𝒖𝒓 𝑭𝒖𝒕𝒖𝒓𝒆</div>
-       <div className='bg-clip-text text-transparent bg-gradient-to-t from-[#fecaca] to-[#ffffff] text-lg sm:text-xl md:text-2xl lg:text-4xl xl:text-5xl flex justify-center mt-3  text-center'>--𝙟𝙤𝙞𝙣 𝙪𝙨 𝙞𝙣 𝙩𝙝𝙚 𝙥𝙪𝙧𝙨𝙪𝙞𝙩 𝙤𝙛 𝙖𝙬𝙚𝙨𝙤𝙢𝙚 𝙖𝙙𝙫𝙚𝙣𝙩𝙪𝙧𝙚𝙨--</div>
+  <div className='text-4xl sm:text-5xl md:text-6xl lg:text-8xl xl:text-9xl bg-clip-text text-transparent bg-gradient-to-t from-[#fcd34d] to-[#ffffff] -mt-20 text-center '> 𝑫𝒆𝒇𝒊𝒏𝒆 𝒀𝒐𝒖𝒓 𝑭𝒖𝒕𝒖𝒓𝒆</div> 
+     <div className='bg-clip-text text-transparent bg-gradient-to-t from-[#fecaca] to-[#ffffff] text-lg sm:text-xl md:text-2xl lg:text-4xl xl:text-5xl flex justify-center mt-3  text-center'>--𝙟𝙤𝙞𝙣 𝙪𝙨 𝙞𝙣 𝙩𝙝𝙚 𝙥𝙪𝙧𝙨𝙪𝙞𝙩 𝙤𝙛 𝙖𝙬𝙚𝙨𝙤𝙢𝙚 𝙖𝙙𝙫𝙚𝙣𝙩𝙪𝙧𝙚𝙨--</div> 
         </div>
         </div>   
         <button 
